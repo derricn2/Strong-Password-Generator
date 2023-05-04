544f8d0bc6e8a5bba8e228e3e4fa3da149200a8d
derricn2 <derricn2@gmail.com>
2023-05-04 12:38:36 -0700

    working on applying functions together, troubleshooted until webpage was fully functional, forgot to mention in previous commit that everything was thoroughly labeled for functionality and practicality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,15 @@
-// minimum character length
+// default/minimum character length
 var charLength = 8;
 // Choice Array
 var chArr = [];
 // Special Character
-var specCharArr = ['$','%','&','^','*','@','!','~'];
+var specCharArr = ['$', '%', '&', '^', '*', '@', '!', '~'];
 // Numbers
-var numArr = ['0','1','2','3','4','5','6','7','8','9'];
+var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Lower Case Alpha
-var lowArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // Upper Case Alpha
-var upArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+var upArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -18,26 +17,35 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var correctPrompts = getPrompts();
+// If prompts filled out correctly, password will be generated 
+  if (correctPrompts) {
+    var customizedPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = customizedPassword;
+  } else {
+    passwordText.value = "";
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// To include prompts to include/exclude various upper/lower case letters, numbers, or special characters
+// Randomizes contents of password
 function generatePassword() {
-
+  var password = "";
+  for (var i = 0; i < charLength; i++) {
+    var randomIndex = Math.floor(Math.random() * chArr.length);
+    password = password + chArr[randomIndex];
+  }
+  return password;
 }
 
+// To include prompts to include/exclude various upper/lower case letters, numbers, or special characters
 // Prompt to choose password characters from 8 - 128
 function getPrompts() {
 
   // Choice Array
-var chArr = [];
+  chArr = [];
 
   charLength = parseInt(prompt("Choose a password length between 8 to 128 characters and click 'Ok'."));
   // Safe net to ensure user follows direction to choose a pw length between 8 - 128
@@ -47,19 +55,19 @@ var chArr = [];
   }
 
   // Prompts if user would like to use Lowercase Letters
-  if(confirm("If you'd like Lowercase Letters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
+  if (confirm("If you'd like Lowercase Letters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
     chArr = chArr.concat(lowArr);
   }
   // Prompts if user would like to use Uppercase Letters
-  if(confirm("If you'd like Uppercase Letters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
+  if (confirm("If you'd like Uppercase Letters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
     chArr = chArr.concat(upArr);
   }
   // Prompts if user would like to use Special Characters
-  if(confirm("If you'd like Special Characters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
+  if (confirm("If you'd like Special Characters in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
     chArr = chArr.concat(specCharArr);
   }
   // Prompts if user would like to use numbers
-  if(confirm("If you'd like Numbers in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
+  if (confirm("If you'd like Numbers in your password, click 'OK' to accept, or 'Cancel' to deny.")) {
     chArr = chArr.concat(numArr);
   }
 
